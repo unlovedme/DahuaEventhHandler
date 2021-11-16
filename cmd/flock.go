7e9d8fa23960a86e5d6d1e717a7d9ccf276e8de4
unlovedme <93517161+unlovedme@users.noreply.github.com>
2021-11-16 09:50:03 +0000

    Fix after merge.

--- cmd/flock.go
+++ cmd/flock.go
@@ -7,4 +7,5 @@ You may obtain a copy of the License at
 
     http://www.apache.org/licenses/LICENSE-2.0
 
-Un
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "A
