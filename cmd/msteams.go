2b0785d3ee3d7f2607b6303a6c0088beaf9bb425
unlovedme <93517161+unlovedme@users.noreply.github.com>
2021-12-17 21:37:46 +0000

    remove released flag

--- cmd/msteams.go
+++ cmd/msteams.go
@@ -11,4 +11,11 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limit
+limitations under the License.
+*/
+
+package cmd
+
+import (
+	"github.com/bitnami-labs/kubewatch/config"
+	"github.com
