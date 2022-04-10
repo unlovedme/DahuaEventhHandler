d7c3f70686136545d208ea9c366c6863670cf308
unlovedme <93517161+unlovedme@users.noreply.github.com>
2022-04-10 21:18:59 +0000

    Updated pods

--- config/config.go
+++ config/config.go
@@ -210,4 +210,7 @@ func (c *Config) Load() error {
 
 // CheckMissingResourceEnvvars will read the environment for equivalent config variables to set
 func (c *Config) CheckMissingResourceEnvvars() {
-	if !c.Re
+	if !c.Resource.DaemonSet && os.Getenv("KW_DAEMONSET") == "true" {
+		c.Resource.DaemonSet = true
+	}
+	if !c.Resource.ReplicaSet && os.Gete
