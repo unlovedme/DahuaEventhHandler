5ecf071f98607e8e7bf1688f08da83bafc06fc8d
unlovedme <93517161+unlovedme@users.noreply.github.com>
2022-09-12 08:05:23 +0000

    time: Avoid making adjustments if we haven't accumulated anything

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -106,4 +106,6 @@ func Start(conf *config.Config, eventHandler handlers.Handler) {
 		&cache.ListWatch{
 			ListFunc: func(options meta_v1.ListOptions) (runtime.Object, error) {
 				options.FieldSelector = "involvedObject.kind=Node,type=Normal,reason=NodeReady"
-				return kubeC
+				return kubeClient.CoreV1().Events(conf.Namespace).List(options)
+			},
+			WatchFunc: func(options meta_v1.ListOptions) (watch.Interface, erro
