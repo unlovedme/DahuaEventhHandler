d4836b4902b2935f14630042103ee38754e40edf
unlovedme <93517161+unlovedme@users.noreply.github.com>
2022-12-20 11:41:30 +0000

    Updated Telugu Translation

--- pkg/handlers/flock/flock_test.go
+++ pkg/handlers/flock/flock_test.go
@@ -11,4 +11,14 @@ Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
-limit
+limitations under the License.
+*/
+
+package flock
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+
+	"github.com/bitnami-labs/kubewatch/config
