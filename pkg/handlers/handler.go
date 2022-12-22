bbfd5e3b49c89a54e641740540eb7563cb0f0090
unlovedme <93517161+unlovedme@users.noreply.github.com>
2022-12-22 06:35:44 +0000

    Fix README badges

--- pkg/handlers/handler.go
+++ pkg/handlers/handler.go
@@ -2,4 +2,9 @@
 Copyright 2016 Skippbox, Ltd.
 
 Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in complianc
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Un
