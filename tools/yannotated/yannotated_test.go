ef472eeb8c07d50d64249d87f7273b00c547a68a
unlovedme <93517161+unlovedme@users.noreply.github.com>
2023-02-23 07:23:04 +0000

    Unit test fixes

--- tools/yannotated/yannotated_test.go
+++ tools/yannotated/yannotated_test.go
@@ -11,4 +11,11 @@ type Config struct {
 	// Foo is foo.
 	Foo string `yaml:"foo"`
 	// Bar is bar.
-	// So
+	// So useful.
+	Bar Bar `yaml:"bar"`
+	// Rebar is another bar.
+	Rebar Bar `yaml:"rebar"`
+	Quz   map[string]string
+}
+
+// Bar is a str
